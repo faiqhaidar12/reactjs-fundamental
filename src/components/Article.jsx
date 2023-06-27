295fb8cd1b3d8b8c12542aa7ab760ba912fd7f44
faiqhaidar1 <faiqhaidar1@gmail.com>
2023-06-27 14:23:34 +0700

    kedua

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,9 +1,14 @@
+const ArticleStatus = ({ isNew }) => {
+  return isNew && <span> Baru!</span>;
+};
+
 function Article(props) {
   return (
     <>
       <h3>{props.title}</h3>
       <small>
         Date : {props.date}, tags : {props.tags.join(", ")}
+        <ArticleStatus isNew={props.isNew} />
       </small>
     </>
   );
